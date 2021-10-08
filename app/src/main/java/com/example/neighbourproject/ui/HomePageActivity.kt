39ca1a96bb5a350cde9444f22f33459c6b06403f
mansah82 <manne.sahlin@iths.se>
2021-10-08 10:27:03 +0200

    cleaned code

--- app/src/main/java/com/example/neighbourproject/ui/HomePageActivity.kt
+++ app/src/main/java/com/example/neighbourproject/ui/HomePageActivity.kt
@@ -1,19 +1,21 @@
-package com.example.neighbourproject
+package com.example.neighbourproject.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+import com.example.neighbourproject.R
 
-lateinit var registerButton: TextView
-lateinit var usernameEdit: EditText
-lateinit var passwordEdit: EditText
 
 class HomePageActivity : AppCompatActivity() {
+
+    private lateinit var registerButton: TextView
+    private lateinit var usernameEdit: EditText
+    private lateinit var passwordEdit: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
@@ -22,42 +24,35 @@ class HomePageActivity : AppCompatActivity() {
         usernameEdit = findViewById(R.id.usernameEditText)
         passwordEdit = findViewById(R.id.passwordEditText)
 
-        emailCheckCorrect()
-
-
-
-
         val loginButton = findViewById<Button>(R.id.loginButton)
-        loginButton.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
 
-        registerButton.setOnClickListener{
+        registerButton.setOnClickListener {
             //Starta skapaaktivitet
         }
 
+        usernameEdit.doAfterTextChanged {
 
-    }
-
-    fun emailCheckCorrect(){ //Work in progress
-
-       val typo =  usernameEdit.getText().toString()
-
-
-        if (typo == "hej") {
-            val mail = usernameEdit.getText().toString();
-            if(!android.util.Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
-                usernameEdit.setError("Please enter a valid username");
-            }
-            else {
-
-                usernameEdit.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_launcher_background, 0)
-
-            }
         }
     }
 
+    fun emailCheckCorrect() { //Work in progress
+        val typo = usernameEdit.getText().toString()
 
+        if (typo == "hej") {
+            val mail = usernameEdit.getText().toString();
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                usernameEdit.setError("Please enter a valid username");
+            } else {
 
+                usernameEdit.setCompoundDrawablesWithIntrinsicBounds(
+                    0, 0,
+                    R.drawable.ic_launcher_background, 0
+                )
+            }
+        }
+    }
 }
