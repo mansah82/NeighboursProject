7d233cfd3d65efcab0b120f077da122ef6ce50b9
mansah82 <manne.sahlin@iths.se>
2021-10-08 12:17:43 +0200

    adding authentication

--- app/src/main/java/com/example/neighbourproject/ui/HomePageActivity.kt
+++ app/src/main/java/com/example/neighbourproject/ui/HomePageActivity.kt
@@ -3,12 +3,15 @@ package com.example.neighbourproject.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import com.example.neighbourproject.R
 import com.example.neighbourproject.ui.search.SearchActivity
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -23,15 +26,35 @@ class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        val auth = Firebase.auth
+
+
+        auth.createUserWithEmailAndPassword("email@jfoe.se", "hejhejhej19")
+
 
         registerButton = findViewById(R.id.registerTextview)
         usernameEdit = findViewById(R.id.usernameEditText)
         passwordEdit = findViewById(R.id.passwordEditText)
+        val TAG = "!!!"
 
         val loginButton = findViewById<Button>(R.id.loginButton)
+
         loginButton.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
+            auth.signInWithEmailAndPassword(usernameEdit.toString(), passwordEdit.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+                }
         }
 
         registerButton.setOnClickListener {
