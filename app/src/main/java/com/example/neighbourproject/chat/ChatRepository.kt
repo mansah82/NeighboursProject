e452219e9071283cdf62ed1d6bc43dd90c6f0bd5
mansah82 <manne.sahlin@iths.se>
2021-11-05 15:20:43 +0100

    Open chatfunction

--- app/src/main/java/com/example/neighbourproject/chat/ChatRepository.kt
+++ app/src/main/java/com/example/neighbourproject/chat/ChatRepository.kt
@@ -1,13 +1,31 @@
 package com.example.neighbourproject.chat
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.neighbourproject.chat.data.ChatMessage
 
 class ChatRepository: ChatService {
-    override fun writeMessage(message: ChatMessage) {
-        TODO("Not yet implemented")
+    companion object {
+        private const val TAG = "ChatRepository"
+
+        private const val CHAT_COLLECTION = "chat"
     }
 
-    override fun getMessages(): List<ChatMessage> {
-        TODO("Not yet implemented")
+
+    private val messageList: MutableList<ChatMessage> = mutableListOf(ChatMessage("jei","Torsten", "Tjena"))
+
+    override fun writeMessage(message: ChatMessage) {
+       //Write message to db
+
+    }
+
+    private fun update(mess: List<ChatMessage>){
+        //Anropas från snapshotlisteners
+        messages.postValue(mess)
+    }
+    private val messages: MutableLiveData<List<ChatMessage>> = MutableLiveData(messageList)
+
+    override fun getLiveMessages(): LiveData<List<ChatMessage>>{
+        return messages
     }
 }
